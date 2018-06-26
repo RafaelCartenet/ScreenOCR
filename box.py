b0755fa32ab9bf0d0fd27e2e17ca4d3ba715c9b7
RafaelCartenet <rafael.cartenet@insa-rouen.fr>
2018-06-26 09:03:32 +0200

    added multiprocessing OCR method

--- box.py
+++ box.py
@@ -2,15 +2,13 @@ import pymouse
 import time
 import pytesseract
 from PIL import ImageGrab
+import multiprocessing as mp
+
 
 class Box:
 
     def __init__(self, lang='en'):
         self.sub_boxes = {}
-        self.x1 = 0
-        self.y1 = 0
-        self.x2 = 0
-        self.y2 = 0
 
     def calibrate(self):
         # Load mouse class
@@ -20,27 +18,26 @@ class Box:
         print '\nPlace the mouse on the TOP LEFT CORNER of the box.'
         raw_input('Press ENTER to capture mouse position')
         print 'DONE!'
-        self.x1, self.y1 = mouse.position()
+        self.tlc_x, self.tlc_y = mouse.position()
 
         print '\nPlace the mouse on the BOTTOM RIGHT CORNER of box.'
         raw_input('Press ENTER to capture mouse position')
         print 'DONE!'
-        self.x2, self.y2 = mouse.position()
+        self.brc_x, self.brc_y = mouse.position()
         print '\n',64*'#','\n\n',19*' ','END OF THE BOX CALIBRATION','\n\n',64*'#'
+
+        self.box = (self.tlc_x, self.tlc_y, self.brc_x, self.brc_y)
         self.update_width_height()
 
     def update_width_height(self):
-        self.width = abs(self.x2 - self.x1)
-        self.height = abs(self.y2 - self.y1)
+        self.width = abs(self.box[2] - self.box[0])
+        self.height = abs(self.box[3] - self.box[1])
 
-    def set_corners(self, coordinates):
-        self.x1= coordinates['x1']
-        self.y1= coordinates['y1']
-        self.x2= coordinates['x2']
-        self.y2= coordinates['y2']
+    def set_box(self, box):
+        self.box = box
         self.update_width_height()
 
-    def load_sub_boxes(self, config_file):
+    def create_sub_boxes(self, config_file):
         """
         Load sub boxes. (x1, y1) and (x2, y2) represent the top left corner of
         the box and the bottom right corner of the box, respectively.
@@ -51,30 +48,96 @@ class Box:
             sub_boxes= f.readlines()
 
         for sub_box in sub_boxes:
-            box_name, x1, y1, x2, y2 = sub_box.split(' ')
+            box_name, tlc_x, tlc_y, brc_x, brc_y = sub_box.split(' ')
+            box = (
+                float(tlc_x)*self.width,
+                float(tlc_y)*self.height,
+                float(brc_x)*self.width,
+                float(brc_y)*self.height
+            )
             self.sub_boxes[box_name] = Box()
-            self.sub_boxes[box_name].set_corners({
-                'x1': self.x1 + float(x1) * self.width,
-                'y1': self.y1 + float(y1) * self.height,
-                'x2': self.x1 + float(x2) * self.width,
-                'y2': self.y1 + float(y2) * self.height,
-            })
+            self.sub_boxes[box_name].set_box(box)
 
-    def get_text(self):
-        grab_time = time.time()
-        image= ImageGrab.grab(bbox=(self.x1, self.y1, self.x2, self.y2))
-        # print 'screenshot', time.time() - grab_time
+    def capture_zones_and_sub_zones(self):
+        # Update main zone capture
+        self.set_capture(ImageGrab.grab())
+        self.update_capture()
 
-        crop_time = time.time()
-        # print 'crop', time.time() - crop_time
+        # Update sub zone captures
+        for box_name in self.sub_boxes:
+            self.sub_boxes[box_name].set_capture(self.box_image)
+            self.sub_boxes[box_name].update_capture()
 
-        OCR_time = time.time()
-        text = pytesseract.image_to_string(image, lang='fra')
-        # print 'OCR', time.time() - OCR_time
+    def set_capture(self, capture):
+        self.box_image = capture
+
+    def update_capture(self):
+        self.box_image = self.box_image.crop(self.box)
+        self.box_image.load()
+
+    def get_text(self, lang='fra'):
+        """
+        OCR. Get text from the box_image
+        - lang: language for tesseract image_to_string
+        """
+        text = pytesseract.image_to_string(self.box_image, lang='fra')
         return text
 
+    def get_text_process(self, image, output):
+        """
+        Parallel process for OCR.
+        - image: PIL image to get text from
+        - output: multiprocessing Queue (mp.Queue())
+        """
+        text = pytesseract.image_to_string(image['image'], lang='fra')
+        result = {
+            'box_name': image['box_name'],
+            'text': text
+        }
+        output.put(result)
 
-    def get_sub_box_text(self, box_name):
-        if box_name not in self.sub_boxes:
-            raise ValueError('No sub box called %s:'% box_name)
-        return self.sub_boxes[box_name].get_text()
+    def parallel_OCR(self, box_names):
+        """
+        Method to extract the text from all the given box_names sub_boxes of the
+        current Box. Uses multiprocessing for efficiency.
+        - box_names: names of the sub_boxes to extract text from
+        return: results: list of OCR for all boxes
+        """
+        self.capture_zones_and_sub_zones()
+
+        # Define an output queue
+        output = mp.Queue()
+
+        # Create image objects, with a box_name key so that we can find them
+        # in the output queue
+        images = []
+        for box_name in box_names:
+            image = {
+                'box_name': box_name,
+                'image': self.sub_boxes[box_name].box_image
+            }
+            images.append(image)
+
+        # Define the process get_text_process to all sub boxes
+        processes = [
+            mp.Process(
+                target=self.get_text_process,
+                args=(image, output)
+            ) for image in images]
+
+        # Run the processes
+        for process in processes:
+            process.start()
+
+        # Exit the completed processes
+        for process in processes:
+            process.join()
+
+        # Get process results from the output queue
+        results = [output.get() for process in processes]
+
+        # Find the box_name key in the output key
+        results = [filter(lambda x: x['box_name'] == box_name, \
+            results)[0]['text'] for box_name in box_names]
+
+        return results
--- test.py
+++ test.py
@@ -1,26 +1,24 @@
-import time
 
-from box import Box
-
-box = Box()
 
 while True:
-    raw_input('Press ENTER to start searching text')
-    box.calibrate()
-    print box.x1, box.y1
-    print box.x2, box.y2
-    print
-    # box.get_text()
 
-# box.calibrate()
-# config_file= 'config/iphone_quicktime_hotspot.txt'
-# box.load_sub_boxes(config_file)
-#
-# while True:
-#     raw_input('Press ENTER to start searching text')
-#     OCR_time = time.time()
-#     print 'question', box.get_sub_box_text('question')
-#     print 'choix1', box.get_sub_box_text('choice1')
-#     print 'choix2', box.get_sub_box_text('choice2')
-#     print 'choix3', box.get_sub_box_text('choice3')
-#     print 'OCR time', time.time() - OCR_time
+    raw_input('Press ENTER to start searching text')
+    OCR_time = time.time()
+
+    box_names = ['question', 'choice1', 'choice2', 'choice3']
+    images = [box.sub_boxes[box_name].get_image() for box_name in box_names]
+
+    print images
+
+    texts = []
+
+
+    print texts
+
+    # print 'question', box.get_sub_box_text('question')
+    # print 'choix1', box.get_sub_box_text('choice1')
+    # print 'choix2', box.get_sub_box_text('choice2')
+    # print 'choix3', box.get_sub_box_text('choice3')
+
+
+    print 'OCR time', time.time() - OCR_time
